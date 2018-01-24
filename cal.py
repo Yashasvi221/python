be90e8040a7ea0ee45e7cd8fa0d0e9bb72bca009
Yashasvi221 <yashasvisingh100@gmail.com>
2018-01-24 14:55:32 +0530

    initial commit

---
+++ cal.py
@@ -0,0 +1,23 @@
+# This is general calculator
+# There are various mathematical function
+# Addition
+x = 3+2
+print(x)
+# Subtarction
+x = 3-2
+print(x)
+# Division
+x = 3/2
+print(x)
+# Multiplication
+x = 3*2
+print(x)
+# Modulas
+x = 3%2
+print(x)
+# Square
+x = 3 ** 2
+print(x)
+# mixed type operands
+x = 4 * 3.75 - 1
+print(x)
